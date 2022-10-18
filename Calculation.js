f6617e52459d42ae95f09a01e65304eed7e717c2
Alphilus <58624681+Alphilus@users.noreply.github.com>
2022-10-18 16:28:17 +0700

    Add files via upload

---
+++ Calculation.js
@@ -0,0 +1,7 @@
+const pi = 3.14
+    let r = prompt('Nhap Ban Kinh');
+    let c = 2* r * pi;
+    let s = r * r * pi;
+    document.write("Chu vi cua hinh tron la: C =" + c);
+    document.write('br/');
+    document.write("Dien tich cua hinh tron la: S =" + s);
